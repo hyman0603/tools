1edbe72ec6b554edfac3d7a2ad0f8758e42e4643
hyman <ywthings@qq.com>
2023-12-11 15:48:09 +0800

    新增主页，多工具选择

--- tools/pages/index/index.js
+++ tools/pages/index/index.js
@@ -1,71 +1,21 @@
+// mainPage.js
+
 Page({
   data: {
-    cells: ["", "", "", "", "", "", "", "", ""],
-    currentPlayer: "X",
-    winner: "",
-    gameEnded: false,
+    tools: [
+      { toolId: "jingziqi", name: "井字棋", logo: "https://mmbiz.qpic.cn/sz_mmbiz_png/DI9jrncbAeDMhzrQiaIY1ibdicsoaEwh0LuY8RXKRsHQ70N0hFlnX4OY44ECfEWNMZpTpJdPkZUJ8ItIcH41pknmQ/640?wx_fmt=png", showLogo: 'Y' },
+      { toolId: "tool2", name: "工具2", logo: "https://mmbiz.qpic.cn/sz_mmbiz_png/DI9jrncbAeDMhzrQiaIY1ibdicsoaEwh0LuY8RXKRsHQ70N0hFlnX4OY44ECfEWNMZpTpJdPkZUJ8ItIcH41pknmQ/640?wx_fmt=png", showLogo: 'Y' },
+      { toolId: "tool3", name: "工具3", logo: "https://mmbiz.qpic.cn/sz_mmbiz_png/DI9jrncbAeDMhzrQiaIY1ibdicsoaEwh0LuY8RXKRsHQ70N0hFlnX4OY44ECfEWNMZpTpJdPkZUJ8ItIcH41pknmQ/640?wx_fmt=png", showLogo: 'N' },
+      { toolId: "tool4", name: "工具4", logo: "https://mmbiz.qpic.cn/sz_mmbiz_png/DI9jrncbAeDMhzrQiaIY1ibdicsoaEwh0LuY8RXKRsHQ70N0hFlnX4OY44ECfEWNMZpTpJdPkZUJ8ItIcH41pknmQ/640?wx_fmt=png", showLogo: 'N' },
+      { toolId: "tool5", name: "工具5", logo: "https://mmbiz.qpic.cn/sz_mmbiz_png/DI9jrncbAeDMhzrQiaIY1ibdicsoaEwh0LuY8RXKRsHQ70N0hFlnX4OY44ECfEWNMZpTpJdPkZUJ8ItIcH41pknmQ/640?wx_fmt=png", showLogo: 'N' }
+      // 添加更多工具
+    ],
   },
 
-  handleCellClick(e) {
-    if (this.data.gameEnded) {
-      return;
-    }
-
-    const {
-      index
-    } = e.currentTarget.dataset;
-    if (!this.data.cells[index] && !this.data.winner) {
-      const cells = this.data.cells.slice(); // 正确获取 cells 变量
-      cells[index] = this.data.currentPlayer;
-
-      this.setData({
-        cells,
-        currentPlayer: this.data.currentPlayer === "X" ? "O" : "X",
-      });
-
-      this.checkWinner(cells, index);
-    }
-
-    if (!this.data.cells.includes("")) {
-      this.setData({
-        gameEnded: true
-      });
-    }
-  },
-
-
-  checkWinner(cells, index) {
-    const winningCombos = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (const combo of winningCombos) {
-      const [a, b, c] = combo;
-      if (cells[a] && cells[a] === cells[b] && cells[a] === cells[c]) {
-        this.setData({
-          winner: cells[a]
-        });
-        this.setData({
-          gameEnded: true
-        });
-        return;
-      }
-    }
-  },
-
-  restartGame() {
-    this.setData({
-      cells: ["", "", "", "", "", "", "", "", ""],
-      currentPlayer: "X",
-      winner: "",
-      gameEnded: false,
+  navigateToTool: function (event) {
+    const toolId = event.currentTarget.dataset.toolid;
+    wx.navigateTo({
+      url: `/pages/${toolId}/${toolId}`,
     });
   },
 });
